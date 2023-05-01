230885f8e14aaeb60b19ce75d0789bc8eb350e2a
Anne Arhipoff <anmaar28421@gmail.com>
2023-05-01 16:13:02 +0300

    add function drawBigWorm
    Draws long worm on the game over view

--- worm.js
+++ worm.js
@@ -36,3 +36,76 @@ function checkWormsLocation(locationToCheck){
     }
     return true;
 }
+
+function drawBigWorm(){
+    document.getElementById("wormHead").style.gridArea = " 11 / 11 / 21 / 21";
+    document.getElementById("wormHead").style.transform = "rotate(" + 90 + "deg)";
+    for(i=2; i<21; i++){
+        //Toprow
+        let tailPiece = document.createElement('div');
+        if(i === 11){
+        tailPiece.className = "turnTailPiece";
+        tailPiece.style.transform = "rotate(" + 270 + "deg)";
+        tailPiece.style.margin = "0.2em 0em 0em 0.3em";
+        tailPiece.style.gridArea = "1 / 11 / 21 / 21";
+        }else{
+        tailPiece.className = "tailPiece";
+        tailPiece.style.gridArea = "1 / " + i + " / 21 / 21";
+        }
+        document.getElementById("gameZone").appendChild(tailPiece);
+        //Bottomrow
+        tailPiece = document.createElement('div');
+        tailPiece.className = "tailPiece";
+        tailPiece.style.transform = "rotate(" + 180 + "deg)";
+        tailPiece.style.gridArea = "21 / " + i + " / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece); 
+        //Right side
+        tailPiece = document.createElement('div');
+        tailPiece.className = "tailPiece";
+        tailPiece.style.transform = "rotate(" + 90 + "deg)";
+        tailPiece.style.gridArea = i + " / 21 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+        //Left side
+        tailPiece = document.createElement('div');
+        tailPiece.className = "tailPiece";
+        tailPiece.style.transform = "rotate(" + 270 + "deg)";
+        tailPiece.style.gridArea = i + " / 1 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+    }
+    //From toprow to head
+    for(i=2; i<11; i++){
+        let tailPiece = document.createElement('div');
+        tailPiece.className = "tailPiece";
+        tailPiece.style.transform = "rotate(" + 90 + "deg)";
+        tailPiece.style.gridArea = i + " / 11 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+    }
+    //Corners 
+    let tailPiece = document.createElement('div');
+        tailPiece.className = "turnTailPiece";
+        tailPiece.style.margin = "0.19em 0.07em 0em";
+        tailPiece.style.gridArea = "1 / 21 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+
+        tailPiece = document.createElement('div');
+        tailPiece.className = "turnTailPiece";
+        tailPiece.style.transform = "rotate(" + 90 + "deg)";
+        tailPiece.style.margin = "0.0em 0.1em 0.1em 0.1em";
+        tailPiece.style.gridArea = "21 / 21 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+
+        tailPiece = document.createElement('div');
+        tailPiece.className = "turnTailPiece";
+        tailPiece.style.transform = "rotate(" + 180 + "deg)";
+        tailPiece.style.margin = "0em 0.1em 0.1em 0.26em";
+        tailPiece.style.gridArea = "21 / 1 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+
+        tailPiece = document.createElement('div');
+        tailPiece.className = "turnTailPiece";
+        tailPiece.style.transform = "rotate(" + 270 + "deg)";
+        tailPiece.style.margin = "0.2em 0em 0em 0.3em";
+        tailPiece.style.gridArea = "1 / 1 / 21 / 21";
+        document.getElementById("gameZone").appendChild(tailPiece);
+
+}
